bc3a8876bb03039afe03d80fbf06ff4506897c2d
yuvalbenshitrit <yuval056@gmail.com>
2024-12-27 15:10:28 +0200

    create swagger

--- dist/src/routes/auth.js
+++ dist/src/routes/auth.js
@@ -6,9 +6,150 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 const auth_1 = __importDefault(require("../controllers/auth"));
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: The Authentication API
+ */
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+/**
+* @swagger
+* components:
+*   schemas:
+*     User:
+*       type: object
+*       required:
+*         - email
+*         - password
+*       properties:
+*         email:
+*           type: string
+*           description: The user email
+*         password:
+*           type: string
+*           description: The user password
+*       example:
+*         email: 'bob@gmail.com'
+*         password: '123456'
+*/
 router.post("/register", auth_1.default.register);
+/**
+* @swagger
+* /auth/register:
+*   post:
+*     summary: registers a new user
+*     tags: [Auth]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/User'
+*     responses:
+*       200:
+*         description: The new user
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/User'
+*     400:
+*      description: Bad request
+*    500:
+*     description: Internal server error
+*/
+/**
+* @swagger
+* components:
+*   schemas:
+*     Tokens:
+*       type: object
+*       required:
+*         - accessToken
+*         - refreshToken
+*       properties:
+*         accessToken:
+*           type: string
+*           description: The JWT access token
+*         refreshToken:
+*           type: string
+*           description: The JWT refresh token
+*       example:
+*         accessToken: '123cd123x1xx1'
+*         refreshToken: '134r2134cr1x3c'
+*/
 router.post("/login", auth_1.default.login);
+/**
+* @swagger
+* /auth/login:
+*   post:
+*     summary: registers a new user
+*     tags: [Auth]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/User'
+*     responses:
+*       200:
+*         description: The acess & refresh tokens
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Tokens'
+*    400:
+*     description: Bad request
+*   500:
+*    description: Internal server error
+*/
+/**
+* @swagger
+* /auth/refreshToken:
+*   get:
+*     summary: get a new access token using the refresh token
+*     tags: [Auth]
+*     description: need to provide the refresh token in the auth header
+*     security:
+*       - bearerAuth: []
+*     responses:
+*       200:
+*         description: The acess & refresh tokens
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Tokens'
+*    400:
+*    description: Bad request
+*  500:
+*   description: Internal server error
+*/
 router.post("/refresh", auth_1.default.refresh);
+/**
+ * @swagger
+ * /auth/logout:
+ *   get:
+ *     summary: logout a user
+ *     tags: [Auth]
+ *     description: need to provide the refresh token in the auth header
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: logout completed successfully
+ *   400:
+ *   description: Bad request
+ * 500:
+ * description: Internal server error
+ */
 router.post("/logout", auth_1.default.logout);
 exports.default = router;
 //# sourceMappingURL=auth.js.map
