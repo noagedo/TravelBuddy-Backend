dc04da4eea7eb1306cc6a8cfc46429cf434e5641
noagedo <gedonoa@gmail.com>
2025-01-25 13:22:18 +0200

    add likesBy to user schema

--- src/models/comment.ts
+++ src/models/comment.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
     postId: { type: mongoose.Schema.Types.String, ref: "Post", required: true },
-    sender: { type: mongoose.Schema.Types.String, ref: "User", required: true },
+    sender: { type: mongoose.Schema.Types.String, ref: "User", required: true },//ID of the user
     content: String,
     createdAt: { type: Date, default: Date.now }
 });
--- src/models/post.ts
+++ src/models/post.ts
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
-    sender: { type: mongoose.Schema.Types.String, ref: "User", required: true },
+    sender: { type: mongoose.Schema.Types.String, ref: "User", required: true },//ID of the user
     content: String,
     createdAt: { type: Date, default: Date.now },
     likes: { type: Number, default: 0 },
+    likesBy: { type: [mongoose.Schema.Types.String], default: [] },
     photos: { type: [String], default: [] }
 })
 
